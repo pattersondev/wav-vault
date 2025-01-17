8196c3ff9ff2a32986aa88c4e30baa4b30f099e8
Sam Patterson <pattersonrsam@gmail.com>
2025-01-17 12:51:35 -0500

    wip

---
+++ client/src/components/app-sidebar.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import * as React from "react";
+import { Music, Upload, Users } from "lucide-react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuItem,
+  SidebarMenuButton,
+} from "@/components/ui/sidebar";
+
+export function AppSidebar() {
+  return (
+    <Sidebar className="border-r">
+      <SidebarHeader>
+        <h2 className="px-6 text-lg font-semibold">Secure Music Transfer</h2>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Menu</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <a href="#">
+                    <Music className="mr-2 h-4 w-4" />
+                    <span>My Files</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <a href="#">
+                    <Upload className="mr-2 h-4 w-4" />
+                    <span>Upload</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <a href="#">
+                    <Users className="mr-2 h-4 w-4" />
+                    <span>Team</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
